83e6d7211b22fb7f8e17492f82eccda292310ec3
moraadiana13 <moraadiana13@gmail.com>
2023-08-11 19:28:40 -0700

    1. Simple shell

---
+++ 0-simple.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main-print Simple shell teamwork
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("Hello, Betty!\n");
+	}
+	return (0);
+}
+
